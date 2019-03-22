eaf28bc8a2fde7ec5858dded88008f8a48b62af1
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-03-22 16:14:42 -0400

    added and extra IP for environement variable

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 WORKDIR=$(pwd)
 docker build -t aink99/ssh-gateway .
 docker rm --force ssh-gw
-docker   run -v  $WORKDIR/www:/www -d    --name=ssh-gw --hostname ssh-gw -e IP=192.168.0.0/24 -p 9080:80 -p 1337:22  -p 8822-8900:8822-8900  aink99/ssh-gateway
+docker   run -v  $WORKDIR/www:/www -d    --name=ssh-gw --hostname ssh-gw -e IP=192.168.0.0/24,172.0.0.0/8 -p 1337:22  -p 9080:80 -p 8822-8900:8822-8900  aink99/ssh-gateway
