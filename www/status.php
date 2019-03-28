d665599550c2a7c00bd92c0e2301b87cb39e4ee2
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-03-28 14:17:48 -0400

    Added a jquery kill ssh to replace the popup window.

--- www/status.php
+++ www/status.php
@@ -103,13 +103,24 @@ echo '
                   <li>PID is '.$pidline[5].' </li>
                   <li>Remote tunnel port is '.$tunneline[6].' </li>
                   <br>
-                  <a> <button type="button" class="btn btn-danger" onclick="window.open(\'/kill.php?pid='.$pidline[5].'\', \'test\', \'width=400, height=400\')"><i class="fa fa-times-circle"></i> Kill ssh session</button>
+                  <!--<a> <button type="button" class="btn btn-danger" onclick="window.open(\'/kill.php?pid='.$pidline[5].'\', \'test\', \'width=400, height=400\')"><i class="fa fa-times-circle"></i> Kill ssh session</button> -->
+                  <a> <button type="button" class="btn btn-danger" onclick="kill();"><i class="fa fa-times-circle"></i> Kill ssh session</button>
                   </ul>
             </div>
         </div>
 <br>
-
+<script type="text/javascript">
+    function kill() {
+        //var jpid = "kill.php?pid=<?php echo $pidline[5]; ?>";
+        var jpid = "kill.php?pid='.$pidline[5].'";
+        console.log(jpid);
+        $.get(jpid);
+        location.reload();
+        return false;
+    }
+    </script>
             ';
+
 
   }
   //Show array
@@ -120,7 +131,7 @@ echo '
 
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"> </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
