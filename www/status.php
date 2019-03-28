1bc76c1338ff663a5d6892c9a4334a893ac2bbe9
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-03-28 13:09:00 -0400

    Cleanup

--- www/status.php
+++ www/status.php
@@ -26,34 +26,20 @@
 <?php
 //Variables
 $file = '/var/log/sshd.log';
-#$searchfor = 'Accepted publickey for';
-$searchfor = 'Accepted publickey for';
+
 // get the file contents, assuming the file to be readable (and exist)
 $contents = file_get_contents($file);
-// escape special characters in the query
-$pattern = preg_quote($searchfor, '/');
-// finalise the regular expression, matching the whole line
-//$pattern = "/^.*$pattern.*\$/m";
-$pattern = "/^.*$pattern.*\n.*\n.*\$/m";
-
-
-//echo '<div class="header1">Hello</div>';
-
 //execute the netstat command and grep for established tcp sessions
 $output = shell_exec('netstat -ant|grep ESTA|grep \:22');
 //echo "<pre>$output</pre>";
-
 // Build an array one netsat tcp line per array content both explode and split are working to indicate then end of the  line
 //$tcp = preg_split('/[\r\n]+/', $output);
 $tcp = explode("\n", $output);
 
-
 //Show  net stat array for debbug
 //print_r($tcp);
 
-
-
-//loop for eacg line/value of the array.
+//loop for each line/value of the array.
 
 foreach($tcp as $value) {
   //Debbug
@@ -104,10 +90,6 @@ foreach($tcp as $value) {
      //print_r($matches);
      $tunnel = implode("\n", $matches[0]);
      $tunneline = preg_split("/[\s,]+/", $tunnel);
-
-
-
-
 
 
 echo '
